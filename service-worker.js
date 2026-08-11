25cd3f83a4281accd4a699f516dc453f7f20effc
MaryJean2 <mate@mary-jean-2.com>
2026-08-11 14:26:41 +0200

    Add files via upload
    added events

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'captains-rotation-v1';
+const CACHE = 'captains-rotation-events-v2';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./default-plan.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event => {
